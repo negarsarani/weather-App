88d2a6d903c703a0bdea7e90b31931190e02e65f
Negar Sarani <negar.sarani2000@gmail.com>
2023-03-30 06:23:10 +0430

    final thursday

--- my-project/src/components/Navbar/Main-header/Status_icon/functions/index.js
+++ my-project/src/components/Navbar/Main-header/Status_icon/functions/index.js
@@ -5,9 +5,3 @@ export function logout(){
     history.pushState("null" ,'null', './login')
     Route()
 } 
-// function deleteCookie(name) {
-//     document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-//   }
-  
-//   // Example usage
-//   deleteCookie("myCookie");
